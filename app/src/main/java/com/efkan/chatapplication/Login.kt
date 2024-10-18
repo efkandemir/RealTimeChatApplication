409844ff74fddd9e55475ea1cc16d2bd547fc626
Efkan Demir <demirefkan33@gmail.com>
2024-10-18 20:29:28 +0300

    Login and signup screens completed

---
+++ app/src/main/java/com/efkan/chatapplication/Login.kt
@@ -0,0 +1,43 @@
+package com.efkan.chatapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.efkan.chatapplication.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.math.log
+
+class Login : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var mAuth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAuth=FirebaseAuth.getInstance()
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+        binding.btnSignup.setOnClickListener{
+            val intent=Intent(this,Signup::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogin.setOnClickListener {
+            val email=binding.edtEmail.text.toString()
+            val password=binding.edtPassword.text.toString()
+            login(email,password)
+        }
+    }
+    private fun login(email: String,password:String){
+    //logic of logging user
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+            if(it.isSuccessful){
+                val intent=Intent(this@Login,MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this@Login,"User does not exist",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/efkan/chatapplication/Signup.kt
@@ -0,0 +1,40 @@
+package com.efkan.chatapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.efkan.chatapplication.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class Signup : AppCompatActivity() {
+    private lateinit var binding:ActivitySignupBinding
+    private lateinit var mAuth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding=ActivitySignupBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+        mAuth=FirebaseAuth.getInstance()
+
+        binding.btnSignupsign.setOnClickListener{
+            val email=binding.edtEmailsign.text.toString()
+            val password=binding.edtPasswordsign.text.toString()
+            signUp(email,password)
+        }
+    }
+    private fun signUp(email:String,password:String){
+        //logic of creating user
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this) {
+            if(it.isSuccessful){
+                val intent=Intent(this@Signup,Login::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this@Signup,"Some error occurred",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
